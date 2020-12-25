1ee4f518c0f4bad557cd468a0b908880800bdd94
youknowkeshi <pongtanate12@gmail.com>
2020-12-25 17:47:36 +0700

    easy

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,18 +1,32 @@
-//Dialog of the first example is given below.
+#include<iostream>
+#include<string>
+using namespace std;
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+int main()
+{
+	string x,m,d,z ;
+	int y;
+	cout << "Fahsai: Sawadee ka...Can you tell me your name?" << "\n" ;
+	cout << "?????: " ;
+	getline(cin,x); 
+	cout << "Fahsai: Wow!!! " <<x<<  " is a really cool name." << "\n";
+	cout << "Fahsai: I think you are an Engineering student. What is your student ID?"<<"\n";
+	cout << x << ": "  ;
+	cin >> y;
+	cin.ignore(); 
+	cout << "Fahsai: I think you may be GEAR " << y/10000000-12 << ". I have a free movie ticket for you." << "\n" ;	
+	cout << "Fahsai: Let's go to the cinema together!!!" << "\n";
+	cout << "Fahsai: What movie do you want to watch?" << "\n";
+	cout << x << ": ";
+	getline(cin,m);
+	cout << "Fahsai: So....which day are you free to go with me?" << "\n";
+	cout << x << ": ";
+	getline(cin,d);
+	cout << "Fahsai: " << d << "....that is OK!!! I'm looking forward to watching " << m << " with you. "<< "\n" ;
+	cout << x << ": ";
+	getline(cin,z);
+	cout << "Fahsai: 555+ see you " << d << ". Bye Bye \\(^ ^)/" ;
+	return 0;
+	
+	
+}
